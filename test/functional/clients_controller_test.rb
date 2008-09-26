241fcdb7db034b7a3365dbf173abc2c31f82ad51
Jonathan Hoyt <jonmagic@Nikita.local>
2008-09-26 15:37:38 -0400

    Trying to get github to my latest version

--- test/functional/clients_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ClientsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
--- test/unit/device_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class DeviceTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
