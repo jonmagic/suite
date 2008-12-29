472612360094d4073b7c31d9bf24c3654c2af75d
Jonathan Hoyt <jonmagic@Nikita.local>
2008-12-28 20:58:06 -0500

    fixed problem in service tag creation due to timezone db issues

--- app/models/device.rb
+++ app/models/device.rb
@@ -14,7 +14,7 @@ class Device < ActiveRecord::Base
   
   def create_service_tag
     if self.service_tag.blank?
-      recent_device = Device.find(:first, :order => 'created_at DESC', :conditions => {:created_at.gt => DateTime.now.beginning_of_day, :created_at.lt => DateTime.now.end_of_day, :device_type_id => self.device_type_id})
+      recent_device = Device.find(:first, :order => 'created_at DESC', :conditions => {:created_at.gt => Time.now.beginning_of_day+5.hours, :created_at.lt => Time.now.end_of_day+5.hours, :device_type_id => self.device_type_id})
       device_type = DeviceType.find(self.device_type_id)
       if recent_device != nil
         service_tag_parts= recent_device.service_tag.split("-")
