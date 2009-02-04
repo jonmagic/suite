a94bd9d28a07fc834bffc3bb63e2adf3e2ed8c3b
Jonathan Hoyt <jonmagic@Sarah.local>
2009-02-04 16:49:37 -0500

    updated time sheet report to include drive time

--- app/controllers/report/time_sheet_controller.rb
+++ app/controllers/report/time_sheet_controller.rb
@@ -7,7 +7,7 @@ class Report::TimeSheetController < ApplicationController
       start_date = params[:start_date] + " 00:00:00"
       end_date = params[:end_date] + " 23:59:59"
       entries = TicketEntry.find(:all, :conditions => {:created_at.gte => start_date, :created_at.lte => end_date, :time.gt => 0})
-      technicians = Hash.new {|h,k| h[k] = {:name => User.find(k).name, :onsite => 0.0, :instore => 0.0, :remote => 0.0, :system_build => 0.0, :billable => 0.0, :non_billable => 0.0}}
+      technicians = Hash.new {|h,k| h[k] = {:name => User.find(k).name, :onsite => 0.0, :instore => 0.0, :remote => 0.0, :system_build => 0.0, :drive_time => 0.0, :billable => 0.0, :non_billable => 0.0}}
       entries.each do |entry|
         if entry.labor_type == "Onsite"
           technicians[entry.creator_id][:onsite] += entry.time
@@ -20,8 +20,16 @@ class Report::TimeSheetController < ApplicationController
         end
         if entry.billable == true
           technicians[entry.creator_id][:billable] += entry.time
+          if !entry.drive_time.blank?
+            technicians[entry.creator_id][:drive_time] += entry.drive_time
+            technicians[entry.creator_id][:billable] += entry.drive_time
+          end
         else
           technicians[entry.creator_id][:non_billable] += entry.time
+          if !entry.drive_time.blank?
+            technicians[entry.creator_id][:drive_time] += entry.drive_time
+            technicians[entry.creator_id][:non_billable] += entry.drive_time
+          end
         end
       end
       @technicians = technicians.values
