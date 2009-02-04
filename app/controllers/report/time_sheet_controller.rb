35af291122493c7abb3422a1e96eebd5809036bd
Jonathan Hoyt <jonmagic@Sarah.local>
2009-02-04 18:40:14 -0500

    removed one labor type, updated the time sheet report

--- app/controllers/report/time_sheet_controller.rb
+++ app/controllers/report/time_sheet_controller.rb
@@ -7,7 +7,16 @@ class Report::TimeSheetController < ApplicationController
       start_date = params[:start_date] + " 00:00:00"
       end_date = params[:end_date] + " 23:59:59"
       entries = TicketEntry.find(:all, :conditions => {:created_at.gte => start_date, :created_at.lte => end_date, :time.gt => 0})
-      technicians = Hash.new {|h,k| h[k] = {:name => User.find(k).name, :onsite => 0.0, :instore => 0.0, :remote => 0.0, :system_build => 0.0, :drive_time => 0.0, :billable => 0.0, :non_billable => 0.0}}
+      technicians = Hash.new {|h,k| h[k] = {
+        :name => User.find(k).name, 
+          :onsite => 0.0,
+          :instore => 0.0,
+          :remote => 0.0,
+          :system_build => 0.0,
+          :warranty => 0.0,
+          :drive_time => 0.0,
+          :billable => 0.0,
+          :non_billable => 0.0}}
       entries.each do |entry|
         if entry.labor_type == "Onsite"
           technicians[entry.creator_id][:onsite] += entry.time
@@ -17,6 +26,8 @@ class Report::TimeSheetController < ApplicationController
           technicians[entry.creator_id][:remote] += entry.time
         elsif entry.labor_type == "System Build"
           technicians[entry.creator_id][:system_build] += entry.time
+        elsif entry.labor_type == "Warranty"
+          technicians[entry.creator_id][:warranty] += entry.time
         end
         if entry.billable == true
           technicians[entry.creator_id][:billable] += entry.time
