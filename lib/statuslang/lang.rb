59ea818c67b79a578638781c18980b6668e185ff
Jonathan Hoyt <jonmagic@Sarah.local>
2009-03-08 10:58:54 -0400

    trying to get the time stuff right on this

--- lib/statuslang/lang.rb
+++ lib/statuslang/lang.rb
@@ -20,7 +20,7 @@ module StatusLang
 
     def posts
       if @options[:after]
-        Event.find(:all, :conditions => {:recordable_type => "Sentry", :recordable_id => @sentry_id, :created_at.gte => @options[:after] + 4.hours})
+        Event.find(:all, :conditions => {:recordable_type => "Sentry", :recordable_id => @sentry_id, :created_at.gte => @options[:after].utc})
       elsif @options[:limit]
         Event.find(:all, :conditions => {:recordable_type => "Sentry", :recordable_id => @sentry_id}, :limit => @options[:limit])
       end
